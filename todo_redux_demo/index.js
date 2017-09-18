c29e9fe9d5732495ef7f9ffbc8a6f8b1a5ca1cf0
zhangsong <449815003@qq.com>
2017-09-18 11:52:29 +0800

    reduces修改

--- todo_redux_demo/index.js
+++ todo_redux_demo/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import  { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import createStore from './reduces';
 import App from './components/app.jsx';
-import TodoApp from './reduces/index.js';
+// import TodoApp from './reduces/index.js';
 
-const store = createStore(TodoApp);       // 里面传三个参数，第一个是reduces，第二个默认值，第三个是中间件等等
+const store = createStore();       // 里面可以传三个参数，第一个是reduces，第二个默认值，第三个是中间件等等
 
 render (
     <Provider store={ store }>
--- todo_redux_demo/reduces/index.js
+++ todo_redux_demo/reduces/index.js
@@ -1,14 +1,24 @@
 /**
- * Created by xiaosong 2017/09/01
+ * Created by xiaosong 2017/09/18
  * **/
 
-import { combineReducers } from 'redux';
+import { applyMiddleware, compose, combineReducers, createStore as createReduxStore } from 'redux';
+import thunk from 'redux-thunk';
 import todos from './todos.js';
 import visibilityFilter from './VisibilityFilter.js';
 
-const TodoApp = combineReducers({
-	todos,
-	visibilityFilter
-})
+const createStore = (initialState = {}) => {
+    const middleware = [thunk]      //中间件的一个流程，此案例没用，可以直接用redux带的createStore
+    const todoApp = combineReducers({
+        todos,
+        visibilityFilter
+    })
+    const store = createReduxStore(
+        todoApp,
+        initialState,
+        applyMiddleware(...middleware)
+    )
+    return store
+}
 
-export default TodoApp
+export default createStore
